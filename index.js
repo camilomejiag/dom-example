d218286c272447f88434827a5524f470014bc8b9
Camilo Mejia <cmejia@hugeinc.com>
2020-02-27 12:58:42 -0500

    Exercise code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const buttonEl = document.querySelector('.button');
+const buttonEl = document.querySelector('.button-input');
 buttonEl.addEventListener('click', () => {
     const textToAdd = 'Hello world';
     const inputEl = document.querySelector('.input-text');
     inputEl.value = textToAdd;
 });
 
+const passwordEl = document.querySelector('.password');
+const confirmPasswordEl = document.querySelector('.confirm-password');
+passwordEl.addEventListener('keyup', function () {
+    // Code goes here
+});
